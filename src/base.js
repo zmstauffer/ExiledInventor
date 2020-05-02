f9bf385eab0ea6a3d185e6639bcf693e9e55649e
zmstauffer <zmstauffer@gmail.com>
2020-05-02 12:13:15 -0400

    Basic level drawing and player movement

---
+++ src/base.js
@@ -0,0 +1,91 @@
+
+
+const width = 10;
+const height = 10;
+const squareSize = 64;
+const playerSpace = 6;
+let playerPos = { x: 1, y:1 };
+
+let root = document.documentElement;
+root.style.setProperty('--squareSize', squareSize + 'px');
+root.style.setProperty('--playerSpace', playerSpace + 'px');
+
+let levelArray = new Array(height);
+
+//hardcode level for now
+levelArray[0] = [1,1,1,1,1,1,1,1,1,1];
+levelArray[1] = [1,0,0,0,0,0,0,0,0,1];
+levelArray[2] = [1,0,1,1,1,1,1,1,0,1];
+levelArray[3] = [1,0,1,0,0,0,0,1,0,1];
+levelArray[4] = [1,0,1,0,0,0,0,1,0,1];
+levelArray[5] = [1,0,1,0,0,0,0,1,0,1];
+levelArray[6] = [1,0,1,0,0,0,0,1,0,1];
+levelArray[7] = [1,0,1,1,1,1,1,1,0,1];
+levelArray[8] = [1,0,0,0,0,0,0,0,0,1];
+levelArray[9] = [1,1,1,1,1,1,1,1,1,1];
+
+let light_mask = [
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,4,4,0,0,0,0,0,0,
+  0,0,0,2,3,4,5,5,4,3,2,0,0,0,
+  0,0,2,3,4,5,6,6,5,4,3,2,0,0,
+  0,2,3,4,5,6,7,7,6,5,4,3,2,0,
+  0,3,4,5,6,7,8,8,7,6,5,4,3,0,
+  0,4,5,6,7,8,9,9,8,7,6,5,4,0,
+  0,4,5,6,7,8,9,9,8,7,6,5,4,0,
+  0,3,4,5,6,7,8,8,7,6,5,4,3,0,
+  0,2,3,4,5,6,7,7,6,5,4,3,2,0,
+  0,0,2,3,4,5,6,6,5,4,3,2,0,0,
+  0,0,0,2,3,4,5,5,4,3,2,0,0,0,
+  0,0,0,0,0,0,4,4,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+];
+
+//make player
+let player = document.createElement('div');
+player.classList.add('player');
+player.style.left = (playerPos.x * squareSize) + playerSpace/2 + 'px';
+player.style.top = playerPos.y * squareSize + playerSpace/2 + 'px';
+document.body.appendChild(player);
+
+//draw level
+for (let y = 0; y < height; y++){
+  for (let x = 0; x < width; x++){
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.classList.add(levelArray[x][y] ? 'wall' : 'walkway');
+    square.style.left = x * squareSize + 'px';
+    square.style.top = y * squareSize + 'px';
+    document.body.appendChild(square);
+  }
+}
+
+window.addEventListener('keydown', function (event) {
+  const key = event.keyCode;
+
+  switch (key) {
+    case 37:        //left
+      playerPos.x--;
+      break;
+    case 38:        //up
+      playerPos.y--;
+      break;
+    case 39:        //right
+      playerPos.x++;
+      break;
+    case 40:        //down
+      playerPos.y++;
+      break;
+  }
+});
+
+function mainLoop(){
+  player.style.left = (playerPos.x * squareSize) + playerSpace/2 + 'px';
+  player.style.top = playerPos.y * squareSize + playerSpace/2 + 'px';
+  requestAnimationFrame(mainLoop);
+}
+
+mainLoop();
+
+
+
